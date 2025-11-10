efc08e6abae9ef2a1e05a0a0f132332a83e814ef
polirritmico <ebray187@gmail.com>
2025-11-09 23:59:59 -0300

    feat: launch the login frame at start

--- src/main/java/cl/edbray/pnb/PixelAndBeans.java
+++ src/main/java/cl/edbray/pnb/PixelAndBeans.java
@@ -1,16 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package cl.edbray.pnb;
 
-/**
- *
- * @author eduardo
- */
+import cl.edbray.pnb.gui.LoginFrame;
+
 public class PixelAndBeans {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        java.awt.EventQueue.invokeLater(() -> {
+            new LoginFrame().setVisible(true);
+        });
     }
 }
