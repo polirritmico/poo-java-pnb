e26bc55d89f704d5b208a1de339bd06b0679ea54
polirritmico <ebray187@gmail.com>
2025-11-10 00:02:22 -0300

    feat(theme): use FlatDarkLaf theme with custom fg

--- src/main/java/cl/edbray/pnb/PixelAndBeans.java
+++ src/main/java/cl/edbray/pnb/PixelAndBeans.java
@@ -1,10 +1,15 @@
 package cl.edbray.pnb;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLaf;
 import cl.edbray.pnb.gui.LoginFrame;
 
 public class PixelAndBeans {
 
     public static void main(String[] args) {
+        FlatLaf.registerCustomDefaultsSource("themes");
+        FlatDarkLaf.setup();
+
         java.awt.EventQueue.invokeLater(() -> {
             new LoginFrame().setVisible(true);
         });
