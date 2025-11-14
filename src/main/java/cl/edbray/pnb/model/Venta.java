c682a08347ed994fbe391f972a51b7dc6bdb72e3
Eduardo Bray <ejbray@uc.cl>
2025-11-13 21:37:13 -0300

    feat: add Venta

---
+++ src/main/java/cl/edbray/pnb/model/Venta.java
@@ -0,0 +1,76 @@
+package cl.edbray.pnb.model;
+
+import java.time.LocalDateTime;
+
+public class Venta {
+    private int id;
+    private LocalDateTime fechaHora;
+    private int usuarioId;
+    private String usuarioNombre;
+    private double total;
+    private String estado;
+    
+    public Venta() {}
+
+    public Venta(int id, LocalDateTime fechaHora, int usuarioId, String usuarioNombre, double total, String estado) {
+        this.id = id;
+        this.fechaHora = fechaHora;
+        this.usuarioId = usuarioId;
+        this.usuarioNombre = usuarioNombre;
+        this.total = total;
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(LocalDateTime fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(int usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public String getUsuarioNombre() {
+        return usuarioNombre;
+    }
+
+    public void setUsuarioNombre(String usuarioNombre) {
+        this.usuarioNombre = usuarioNombre;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    @Override
+    public String toString() {
+        return "Venta #" + id + " - $" + String.format("%,.0f", total);
+    }
+}
