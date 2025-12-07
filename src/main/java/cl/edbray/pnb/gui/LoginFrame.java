2bef143beafe4ad8edea1ec89306c7ecb60b08c3
polirritmico <ebray187@gmail.com>
2025-12-06 22:17:54 -0300

    hotfix: re-enable pseudo pass

--- src/main/java/cl/edbray/pnb/gui/LoginFrame.java
+++ src/main/java/cl/edbray/pnb/gui/LoginFrame.java
@@ -207,8 +207,8 @@ public class LoginFrame extends javax.swing.JFrame {
         String username = usernameField.getText();
         String password = new String(passwordField.getPassword());
 
-        //if (username.equals("admin") && password.equals("1234")) {
-        if (true) {
+        if (username.equals("admin") && password.equals("1234")) {
+        //if (true) {
             MainFrame main = new MainFrame();
             main.setVisible(true);
             this.dispose();
@@ -228,7 +228,7 @@ public class LoginFrame extends javax.swing.JFrame {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
