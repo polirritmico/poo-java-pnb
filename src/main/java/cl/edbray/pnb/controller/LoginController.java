041bf86887b95c9df6b6377dc5e9d1cd6374b161
polirritmico <ebray187@gmail.com>
2025-12-07 01:14:13 -0300

    feat(LoginController): add base controller

---
+++ src/main/java/cl/edbray/pnb/controller/LoginController.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cl.edbray.pnb.controller;
+
+import cl.edbray.pnb.model.User;
+import cl.edbray.pnb.service.UserService;
+
+/**
+ *
+ * @author eduardo
+ */
+public class LoginController {
+    private final UserService userService;
+
+    public LoginController(UserService userService){
+        this.userService =userService;
+    }
+
+    public User authenticate(String username, String password) {
+        return userService.authenticate(username, password);
+    }
+}
