0c5a0c9dc5d9036ae47391aa54e66d7969fe7062
polirritmico <ebray187@gmail.com>
2025-12-08 14:36:27 -0300

    chore: move test packages to utils/tests

--- src/main/java/cl/edbray/pnb/utils/tests/TestDBConnection.java
+++ src/main/java/cl/edbray/pnb/utils/tests/TestDBConnection.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package cl.edbray.pnb.utils;
+package cl.edbray.pnb.utils.tests;
 
+import cl.edbray.pnb.utils.DatabaseConnectionFactory;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
--- src/main/java/cl/edbray/pnb/utils/tests/TestSaleRepository.java
+++ src/main/java/cl/edbray/pnb/utils/tests/TestSaleRepository.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package cl.edbray.pnb.utils;
+package cl.edbray.pnb.utils.tests;
 
 import cl.edbray.pnb.model.Sale;
 import cl.edbray.pnb.repository.SaleRepository;
